838c52a36bfba12e42a0a13a2bfce620c9a55726
ruuuu <rufinka_91@mail.ru>
2022-10-12 16:41:14 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+
+const formatCurrency = (n) => {
+      const currency = new Intl.NumberFormat('ru-RU', {
+            style: 'currency',
+            currency: 'RUB',
+            maximumFractionDigits: 2 //  сколкьо знков после запятой
+      });
+
+      return currency.format(n);
+
+}
+
+
+
+
+
+
+
 const navigationLinks = document.querySelectorAll('.navigation__link');       // псевдомассив(NodeList-спсиок нод, каждый элемент на станице это нода) ссылок меню [a.navigation__link.navigation__link_active, a.navigation__link]
 const calcElems = document.querySelectorAll('.calc');                         // [ section, section, section, section]
 
@@ -23,7 +41,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
 }
 
 
-
+// КАкулятор АУСН: 
 const ausn = document.querySelector('.ausn');                                 // калькудтор ausn <section></section>
 const formAusn = ausn.querySelector('.calc__form');                            //   форма
 const resultTaxTotal = ausn.querySelector('.result__tax--total');
@@ -41,10 +59,10 @@ formAusn.addEventListener('input', () => {                                    //
       if (formAusn.type.value === 'income') {                                       // <input name="type">  если выбрали первый чекбокс
             calcLabelExpenses.style.display = 'none';
             formAusn.income.value = '';
-            resultTaxTotal.textContent = formAusn.income.value * 0.08;
+            resultTaxTotal.textContent = formatCurrency(formAusn.income.value * 0.08);
       }
       if (formAusn.type.value === 'expenses') {                                     // <input name="type">  если выбрали первый чекбокс
-            resultTaxTotal.textContent = (formAusn.income.value - formAusn.expenses.value) * 0.2;
+            resultTaxTotal.textContent = formatCurrency((formAusn.income.value - formAusn.expenses.value) * 0.2);
             calcLabelExpenses.style.display = 'block';
       }
 
@@ -57,12 +75,44 @@ formAusn.addEventListener('input', () => {                                    //
 const selfEmployment = document.querySelector('.self-employment');                        // <section>
 const formSelfEmployment = selfEmployment.querySelector('.calc__form--selfemployment');                            //   форма
 const resultTaxSelfEmpolyment = selfEmployment.querySelector('.result__tax--selfemployment');
+const calcCompensation = selfEmployment.querySelector('.calc__label_compensation');   // поле Остаток вычета
+const resultBlockCompensation = selfEmployment.querySelectorAll('.result__block--compensation'); // [div,div,div]
+
+const resultTaxCompensation = selfEmployment.querySelector('.result__tax--compensation');
+const resultTaxRestCompensation = selfEmployment.querySelector('.result__tax--rest-compensation');
+const resultTaxResult = selfEmployment.querySelector('.result__tax--result');
 
 
+const checkCompensation = () => {
+      const setDisplay = formSelfEmployment.addCompensation.checked ? 'block' : 'none';
+      calcCompensation.style.display = setDisplay;
 
-formSelfEmployment.addEventListener('input', () => {                                     //  когда будем вводить символ в поле, сработает событие
+      resultBlockCompensation.forEach((elem, i, array) => {               // перебираем массив, метод применяет переданную фукнцию для каждлого элемета массива
+            elem.style.display = setDisplay;
+      });
+
+}
+
+
+checkCompensation();                                                                      // нач значение
+
+
+formSelfEmployment.addEventListener('input', () => {                                     //  когда будем вводить символ в поле, сработает событие 'input'
       const resIndividual = formSelfEmployment.incomeFizik.value * 0.04;
       const resEntity = formSelfEmployment.incomeLower.value * 0.06;
-      resultTaxSelfEmpolyment.textContent = resIndividual + resEntity;
+      checkCompensation();
+
+      const tax = resIndividual + resEntity;
+      formSelfEmployment.compensation.value > 10000 ? 10000 : formSelfEmployment.compensation.value;
+
+      const benefit = formSelfEmployment.compensation.value;                              //  поле Остаток вычета
+      const resBenefit = formSelfEmployment.incomeFizik.value * 0.01 + formSelfEmployment.incomeLower.value * 0.02;
+      const finalBenefit = benefit - resBenefit > 0 ? (benefit - resBenefit) : 0;
+      const finalTax = tax - (benefit - finalBenefit);
+
+      resultTaxSelfEmpolyment.textContent = formatCurrency(tax);
+      resultTaxCompensation.textContent = formatCurrency(benefit - finalBenefit);
+      resultTaxRestCompensation.textContent = formatCurrency(finalBenefit);
+      resultTaxResult.textContent = formatCurrency(finalTax);
 
 });
